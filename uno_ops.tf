82bcd27296b0a114bae26f027753449b8b07f566
Nicholas Jackson <njackson.gis@gmail.com>
2020-08-12 17:21:05 -0400

    fixed the bucket name and region

--- uno_ops.tf
+++ uno_ops.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   profile = "default"
-  regions = "us-east-1"
+  region  = "us-east-1"
 }
 
 resource "aws_s3_bucket" "main_bucket" {
-  bucket = "dev_20200812"
+  bucket = "dev-naj-tf-20200812"
   acl    = "private"
 }
